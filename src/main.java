b7af19560b750faa3f939040772bd36bfd71181c
grosalex <alexbruneau93@gmail.com>
2014-05-08 00:49:46 +0200

    adding the todo list

--- src/main.java
+++ src/main.java
@@ -2,7 +2,15 @@ import View.Window;
 
 
 public class main {
-
+/*
+ * TODO 
+ * Hospitalisation
+ * toutes les recherches externes aux personnes
+ * delete
+ * modifier
+ * rendre ça joli
+ * javadocs
+ */
 	public static void main(String[] args) {
 		Window main_window= new Window();
 		
