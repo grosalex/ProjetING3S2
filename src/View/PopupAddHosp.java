82dfac17bfebc5a2fd5798d8d76e51cbae724a07
grosalex <alexbruneau93@gmail.com>
2014-05-08 16:23:06 +0200

    there is an issue

--- src/View/PopupAddHosp.java
+++ src/View/PopupAddHosp.java
@@ -13,36 +13,33 @@ import model.NoResultException;
 import model.Patient;
 import model.Resultat;
 
-public class PopupAddHosp extends PopupAddPatient{
+public class PopupAddHosp extends PopupAddPerson{
+	private JLabel mutuel_label = new JLabel("mutual");
 	private JLabel doctor_id_label=new JLabel("Id doctor :");
 	private JLabel service_id_label = new JLabel("Id service :");
+	private JTextField mutuel = new JTextField();
 	private JTextField doctor_id = new JTextField();
 	private JTextField service_id = new JTextField();
 	public PopupAddHosp(Window current_window) {
 		super(current_window);
+
+
 		this.main_panel.remove(this.second_panel);
+		this.main_panel.add(mutuel_label);
+		this.main_panel.add(mutuel);
 		this.main_panel.add(doctor_id_label);
 		this.main_panel.add(doctor_id);
 		this.main_panel.add(service_id_label);
 		this.main_panel.add(service_id);
 		this.main_panel.add(this.second_panel);
+
+		this.add(this.main_panel);
 		this.save.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					Patient current_patient = new Patient(name.getText(), surname.getText(), phone.getText(), address.getText(),0, speciality.getText());
+					Patient current_patient = new Patient(name.getText(), surname.getText(), phone.getText(), address.getText(),0, mutuel.getText());
 					Add.addHop(current_patient, Integer.parseInt(doctor_id.getText()), service_id.getText());
-/*					String requete_doc = "SELECT employe.numero ,nom, prenom, adresse, tel, specialite FROM employe,docteur WHERE employe.id="+doctor_id.getText();
-					String requete_service = "SELECT code ,nom, batiment, directeur FROM service WHERE code="+service_id.getText();
-					try {
-						Resultat doc_resultat = new Resultat(Connexion.getInstance(), requete_doc);
-						Resultat service_resultat = new Resultat(Connexion.getInstance(), requete_service);
-						Doctor current_doc=new Doctor(doc_resultat.getResult()[0][0]);						
-					} catch (NoResultException e1) {
-						// TODO Bloc catch généré automatiquement
-						e1.printStackTrace();
-					}
-					*/
 					setVisible(false);
 					
 				} catch (SQLException e1) {
